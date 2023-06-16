9f4c40a9c1bdc9987270ebc3f10f54de32289c2d
kaito kitamura <d1j3m1n3@gmail.com>
2023-06-16 20:43:42 +0900

    first commit

---
+++ CloneApp/TinderClone/TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by 喜多村海人 on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
